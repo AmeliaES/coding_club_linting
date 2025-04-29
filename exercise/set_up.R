d2e6c8323c9cc1bd2992aa0de9d437753e7b1dac
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-04-29 16:11:25 +0100

    update exercises

--- exercise/set_up.R
+++ exercise/set_up.R
@@ -8,7 +8,27 @@ install.packages("styler")
 library(here)
 library(lintr)
 
+# Before we get started Read The Documentation (RTD!) for the lintr package:
+# https://lintr.r-lib.org/articles/lintr.html 
+# This will tell you useful info about the functions and how to use them.
+
+# Exercises:
+# 1. Using the `lint` function lint either your own script or one of the provided scripts.
 
 # Example to lint one of my scripts:
 lint(here("exercise/supplementary_tables_excel.R"))
+# Hint: alternative to running the above you can click on "Addins" 
+# and under the "LINTR" heading click on "lint current file".
 
+# 2. Go through the linting results and fix a couple of the issues that are raised 
+# (if you're stuck see my video here: https://youtu.be/I736wZs_QiE).
+
+# 3. By default lintr uses the R tidyverse style. 
+# Create a lintr configuration file and add a new linter. 
+# (Tip: Read The Documentation: https://lintr.r-lib.org/articles/lintr.html#configuring-linters)
+
+# 4. Try turning off the line_length_linter to remove any linting warnings about line length.
+
+# 5. Try and exclude certain lines of code from linting.
+
+# 6. Automate linting on every push to GitHub, using GitHub actions (Tip: Read The Documentation: )
